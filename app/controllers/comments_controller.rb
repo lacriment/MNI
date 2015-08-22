c68e5610be7f60ee7e9d95532ddb8fa2a6951422
Burak KIYAK <brkkyk@gmail.com>
2015-08-22 08:40:10 +0300

    Views updated, dropdown menu and other items added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,6 @@ class CommentsController < ApplicationController
 
   def create
     @comment = current_user.comments.create(comment_params)
-
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment.movie, notice: 'Comment was successfully created.' }
--- db/schema.rb
+++ db/schema.rb
@@ -93,4 +93,11 @@ ActiveRecord::Schema.define(version: 20150821223330) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "wishlists", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
